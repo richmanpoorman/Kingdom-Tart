bd78b61e80e06ca471670c3b79d9073b122e70cb
Matthew <Matthew@TheBeastMKW>
2019-10-17 22:11:13 -0400

    10 / 17 / 2019 Update 2 Development of the Main class - Made the gameplay

--- Enemy.java
+++ Enemy.java
@@ -8,8 +8,8 @@ public class Enemy extends Character{
 	//Constructors
 	public Enemy(String name, int x, int y, int hp, int dmg, int width, int height) {
 		super(name, x, y, width, height, hp, dmg);
-		if(!dictionary.containsKey(name))
-			dictionary.put(name, new Enemy(name, 0, 0, hp, dmg, width, height));
+	//	if(dictionary.containsKey(name) == false);
+	//		dictionary.put(name, new Enemy(name, 0, 0, hp, dmg, width, height));
 	}
 	public Enemy(String name, int x, int y) {
 		super(name,x,y,dictionary.get(name).width,dictionary.get(name).height,dictionary.get(name).hp,dictionary.get(name).dmg);
--- Main.java
+++ Main.java
@@ -6,30 +6,33 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import java.awt.event.KeyEvent;
+//import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-import java.awt.event.KeyListener;
+//import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
-public class Main extends JPanel implements Runnable, ComponentListener, MouseMotionListener, KeyListener{
+public class Main extends JPanel implements Runnable{
 	public MainCharacter player;
 	public MainCharacter[] party = new MainCharacter[4];
 	public ArrayList<MainCharacter> teammates = new ArrayList<MainCharacter>();
 	
-	
+	public int turn = 0;
+	public boolean isTurn = true;
 	
 	public static Set<Integer> pressed;
 	
 	public Tileset map;
-	public Enemy[] enemy;
+	public Enemy[] enemy = new Enemy[4];
 	public int[] mousePos = new int[2];
 	public ArrayList<Enemy> enemiesInField = new ArrayList<Enemy>();
-	public boolean isBattle = false;
+	public boolean isBattle = true;
 	
+	public static HashMap<String,Animation> anim = new HashMap<String, Animation>();
 	
 	/* Class to return images: stores images into "animations"  */
 	public class Animation{
@@ -71,11 +74,13 @@ public class Main extends JPanel implements Runnable, ComponentListener, MouseMo
 		//adds all the components to look for
 		addKeyListener(new KeyListener());
 		addMouseListener(new MouseListener());
+		enemy[0] = new Enemy ("John Adams", 0, 0, 100, 100, 100, 100);
+		player = new MainCharacter("player",100,100,10,10,10,10); // Stock Character
+		player.setRecipes("", "", "", "");
 		
-		player = new MainCharacter("player",100,100,10,10,10,10);
-		
+		party[0] = player;
 		pressed = new HashSet<Integer>();
-		setFocusable(true);
+		setFocusable(true); // Allows the program to focus on different types of inputs
 		setDoubleBuffered(false);
 		
 		// Starts runnable
@@ -83,21 +88,9 @@ public class Main extends JPanel implements Runnable, ComponentListener, MouseMo
 		t.start(); 
 		
 	}
+
 	
-	public void gameplay() {
-		
-		if (isBattle) {
-			
-			
-			
-		}
-		else {
-			
-			player.playerUpdate(pressed);
-			
-		}
-		
-	}
+
 	
 	
 	
@@ -107,7 +100,9 @@ public class Main extends JPanel implements Runnable, ComponentListener, MouseMo
 		// Frames of different gameplay
 		if (isBattle) {
 			
-			
+			for(int i = 0; i < 4; i ++ ) {
+				g.fillRect(10 + 110 * i, 400, 100, 100);
+			}
 			
 		}
 		else {
@@ -141,31 +136,209 @@ public class Main extends JPanel implements Runnable, ComponentListener, MouseMo
 			}
 		}
 	}
-	@Override
-	public void componentHidden(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public boolean isBetween(int posX, int posY, int x, int w, int y, int h) {
+		return (posX >= x && posX <= x + w && posY >= y && posY <= y + h);
 	}
-	@Override
-	public void componentMoved(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
+	
+	
+	
+	public void gameplay() {
 		
-	}
-	@Override
-	public void componentResized(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void componentShown(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
+		if (isBattle) { // Checks if in battle
+			if(isTurn) { // Your turn
+				if(party[turn] != null) { // if there is a party member
+					if(isBetween(mousePos[0], mousePos[1], 10, 100, 400, 100)) { // If in atk block 1
+						mousePos[0] = 10; // Locks mouse x movement
+						mousePos[1] = 400; // Locks mouse y movement
+						System.out.println("ATK 1");
+						
+						
+						if(!anim.isEmpty() && anim.containsKey(party[turn].name)) { // If there is an animation
+							if(anim.get(party[turn].name).isPlayed()) { // if the animation has played, go to the next character
+								
+								// // If the party member exists
+									if(party[turn].recipes[0] != null) // If they have a function
+										party[turn].recipes[0].interact(); // Do it
+								
+								turn++; // next person
+								// Mouse reset
+								mousePos[0] = 0;
+								mousePos[1] = 0;
+	
+								
+							}
+						}
+						else { // if there is no animation, just go to the next character
+							
+							// // If the party member exists
+								if(party[turn].recipes[0] != null) // If they have a function
+									party[turn].recipes[0].interact(); // Do it
+							
+							turn++; // next person
+							// Mouse reset
+							mousePos[0] = 0;
+							mousePos[1] = 0;
+	
+						}
+						
+				
+					
+					}
+					else if(isBetween(mousePos[0], mousePos[1], 120, 100, 400, 100)) { // If in atk block 2
+						mousePos[0] = 120;
+						mousePos[1] = 400;
+						System.out.println("ATK 2");
+						if(!anim.isEmpty() && anim.containsKey(party[turn].name)) {
+							
+							// // If the party member exists
+								if(party[turn].recipes[0] != null) // If they have a function
+									party[turn].recipes[0].interact(); // Do it
+							
+							if(anim.get(party[turn].name).isPlayed()) {
+								turn++; // next person
+								// Mouse reset
+								mousePos[0] = 0;
+								mousePos[1] = 0;
+							}
+	
+						}
+						else {
+							
+							// // If the party member exists
+								if(party[turn].recipes[0] != null) // If they have a function
+									party[turn].recipes[0].interact(); // Do it
+							
+							turn++; // next person
+							// Mouse reset
+							mousePos[0] = 0;
+							mousePos[1] = 0;
+	
+						}
+				
+					
+					}
+					else if(isBetween(mousePos[0], mousePos[1], 230, 100, 400, 100)) { // If in atk block 3
+						mousePos[0] = 230;
+						mousePos[1] = 400;
+						System.out.println("ATK 3");
+						
+						
+						if(!anim.isEmpty() && anim.containsKey(party[turn].name)) {
+							if(anim.get(party[turn].name).isPlayed()) {
+								
+								// // If the party member exists
+									if(party[turn].recipes[0] != null) // If they have a function
+										party[turn].recipes[0].interact(); // Do it
+								
+								turn++; // next person
+								// Mouse reset
+								mousePos[0] = 0;
+								mousePos[1] = 0;
+	
+							}
+						}
+						else {  // if there is no animation, just go to the next character
+							
+							// // If the party member exists
+								if(party[turn].recipes[0] != null) // If they have a function
+									party[turn].recipes[0].interact(); // Do it
+							
+							turn++; // next person
+							// Mouse reset
+							mousePos[0] = 0;
+							mousePos[1] = 0;
+							
+						}
+					
+					
+					}
+					else if(isBetween(mousePos[0], mousePos[1], 340, 100, 400, 100)) { // If in atk block 4
+						mousePos[0] = 340;
+						mousePos[1] = 400;
+						System.out.println("ATK 4");
+						
+						
+						if(!anim.isEmpty() && anim.containsKey(party[turn].name)) {
+							if(anim.get(party[turn].name).isPlayed()) {
+								
+								// // If the party member exists
+									if(party[turn].recipes[0] != null) // If they have a function
+										party[turn].recipes[0].interact(); // Do it
+								
+								turn++; // next person
+								// Mouse reset
+								mousePos[0] = 0;
+								mousePos[1] = 0;
+	
+							}
+						}
+						else {// if there is no animation, just go to the next character
+							
+							// // If the party member exists
+								if(party[turn].recipes[0] != null) // If they have a function
+									party[turn].recipes[0].interact(); // Do it
+							
+							turn++; // next person
+							// Mouse reset
+							mousePos[0] = 0;
+							mousePos[1] = 0;
+							
+							
+	
+						}
+					}
+				}
+				else { // if no member, skip
+					turn ++;
+				}
+				
+				
+				//If gone through all the party members
+				if(turn >= party.length) {
+					isTurn = false;
+					
+					turn = 0;
+				}
+				
+				
+				
+				
+			}
+			else { // Enemy Turn
+				isTurn = true; // Testing: player turn;
+				System.out.println("Enemy");
+				
+			}
+			
+			
+			// Checks enemy hp
+			if(enemy == null) // Checks if the enemy have people
+				isBattle = false;
+			else
+				for(Enemy e : enemy)
+					if(e != null)
+						if(e.hp <= 0)
+							isBattle = false;
+			if(party == null) // Checks if the party has people
+				isBattle = false;
+			else
+				for(MainCharacter m : party)
+					if(m!= null)
+						if(m.hp <= 0)
+							isBattle = false;
+		}
+		else { // Not in combat
+			
+			player.playerUpdate(pressed); // Momentary: creates a square where the Main Character is
+			
+		}
 		
 	}
 	
 	private class KeyListener extends KeyAdapter{
 		@Override
 		public synchronized void keyPressed(KeyEvent e) {
-			System.out.println("Press");
+			// System.out.println("Press"); // Test the Key Function
 			pressed.add(e.getKeyCode());
 		}
 
@@ -180,7 +353,7 @@ public class Main extends JPanel implements Runnable, ComponentListener, MouseMo
 		@Override
 		public void mousePressed(MouseEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("Click");
+			// System.out.println("Click"); // Test the Press Function
 			mousePos[0] = e.getX();
 			mousePos[1] = e.getY();
 		}
@@ -192,33 +365,4 @@ public class Main extends JPanel implements Runnable, ComponentListener, MouseMo
 		}
 	}
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 }
